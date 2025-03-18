LineageOS/android_device_xiaomi_msm8998-common

msm8998-common: turn fingerprint wrapper intro a hidl service
Change-Id: Ib72892417d401947aeb3935ef0f018c3b5daf94b

msm8998.mk
@@ -181,8 +181,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
-    fingerprint.msm8998
+    android.hardware.biometrics.fingerprint@2.1-service.msm8998
 
 # For config.fs
 PRODUCT_PACKAGES += \

bmwtheseries7/device_xiaomi_msm8998-common

msm8998.mk
@@ -181,8 +181,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
-    fingerprint.msm8998
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8998
 
 # For config.fs
 PRODUCT_PACKAGES += \